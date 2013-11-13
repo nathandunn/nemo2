c81cbb30d540bb99a586e8153a5cd6c56cb13eb9
Nathan Dunn <ndunn@uoregon.edu>
2013-11-13 08:53:13 -0800

    updated

---
+++ test/unit/edu/uoregon/nic/nemo/portal/ConditionServiceTests.groovy
@@ -0,0 +1,13 @@
+package edu.uoregon.nic.nemo.portal
+
+import grails.test.mixin.TestFor
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ConditionService)
+class ConditionServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
